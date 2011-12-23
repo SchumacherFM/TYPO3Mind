d3af5a67419abeee45947061991d4ea749ff6c53
Cyrill Schumacher <cyrill@schumacher.fm>
2011-12-23 15:02:03 +0100

    Copyright notice

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TYPO3Mind',
-	'description' => 'TYPO3Mind is an extension for generating mind mapping files from your whole TYPO3 installation. Mind maps helps you to understand how your TYPO3 project has been setup and what the current running status is. Currently you can only export .mm files which can be imported by FreeMind, Freeplane, XMind, Mindjet, MindManager, etc. TYPO3Mind uses the cool icon from FreeMind. This extension hooks into the tree click menu and in the left pane. The mind map includes many icons and pictures with URIs to your webserver. You have a lot of configuration options. Needs TYPO3 4.5 or later, SimpleXML, PHP5. Export via eID is also possible.',
+	'description' => 'TYPO3Mind is an extension for generating mind mapping files from your whole TYPO3 installation. Mind maps helps you to understand how your TYPO3 project has been setup and what the current running status is. Currently you can only export .mm files which can be imported by FreeMind (strongly recommended), Freeplane, XMind, Mindjet, MindManager, etc. TYPO3Mind uses the cool icon from FreeMind. This extension hooks into the tree click menu and in the left pane. The mind map includes many icons and pictures with URIs to your webserver. You have a lot of configuration options. Needs TYPO3 4.5 or later, SimpleXML, PHP5.2 or later. Export via eID is also possible.',
 	'category' => 'be',
 	'author' => 'Cyrill Schumacher',
 	'author_email' => 'Cyrill@Schumacher.fm',
